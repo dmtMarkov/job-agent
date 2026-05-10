bcbe6cf1499f7493e752914e9d3160f4341048d1
dmitry <dmitry.m.v.ds@gmail.com>
2026-05-10 14:07:21 +0500

    feat: refactor project structure

---
+++ db/connection.py
@@ -0,0 +1,12 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
+
+conn = get_connection()
+print("Подключение успешно!")
+conn.close()
